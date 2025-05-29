9abdbd38cb9d3a9178fd2cb9b226c1c45e521676
Julio <Julio@DESKTOP-SSKL7Q9>
2025-05-29 19:28:28 -0300

    Alterações no Projeto Hotel

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+
+        public int QtdAdultos { get; set; }
+        public int QtdCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadia
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QtdAdultos * QuartoSelecionado.ValorDiariaAdulto;
+                double valor_criancas = QtdCriancas * QuartoSelecionado.ValorDiarioCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadia;
+
+                return total;
+            }
+
+        }
+    }
+}
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -24,15 +26,28 @@ public partial class ContratacaoHospedagem : ContentPage
 		await Navigation.PushAsync(new Sobre());
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+            Hospedagem h = new Hospedagem()
+            {
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+                QtdAdultos = Convert.ToInt32(stp_adultos.Value),
+                QtdCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date,
+
+            };
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
 
         }catch (Exception ex)
         {
-            DisplayAlert("Ops", ex.Message, "Fechar");
+           await DisplayAlert("Ops", ex.Message, "Fechar");
         }
     }
 
